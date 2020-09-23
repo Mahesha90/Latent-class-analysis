d6c5c0ca4b09d9abe230560d8e60b2a52f1e0620
Mahesha90 <62265187+Mahesha90@users.noreply.github.com>
2020-09-23 17:57:28 +0300

    Commit

--- test.R
+++ test.R
@@ -1,3 +1,19 @@
 install.packages("GPArotation")
 install.packages("nFactor")
 install.packages("psych")
+install.packages("lattice")
+library(GPArotation)
+library(lattice)
+library(nFactors)
+library(psych)
+
+data <- read.delim(file.choose(),header = TRUE)
+data.pca <- prcomp(data)
+summary(data.pca)
+
+parallel<-fa.parallel(data, fm='minres', fa='fa')
+
+threefactor <- fa(data,nfactors = 3,rotate = "oblimin",fm="minres")
+print(threefactor)
+
+print(threefactor$loadings,cutoff = 0.3, sort=TRUE)
